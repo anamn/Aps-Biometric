c32921e4066fd4ecd2cd5e0e4a184feb13fcba11
anamn <anamacielnasc13@gmail.com>
2021-10-26 10:00:47 -0300

    Projeto inicial

---
+++ Opencv.py
@@ -0,0 +1,17 @@
+import cv2
+
+loadAlgorithm = cv2.CascadeClassifier('Haarcascade/haarcascade_frontalface_default.xml')
+
+image = cv2.imread('Images/image1.jpg')
+
+grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+faces = loadAlgorithm.detectMultiScale(grayImage)
+
+print(faces)
+
+for(x, y, l, a) in faces:
+    cv2.rectangle(image, (x, y), (x + l, y + a), (255, 0, 255), 2)
+
+cv2.imshow("Faces", image)
+cv2.waitKey()
